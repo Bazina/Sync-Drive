1711a7d2f936d17b32029462c9b8920b65deda68
bazina <youssefbazina7@gmail.com>
2024-01-23 16:33:20 +0200

    Use Tokens

--- sync_drive.py
+++ sync_drive.py
@@ -1,19 +1,25 @@
 import json
 import os
+import io
+import shutil
 
-import pydrive.files
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QTextCursor, QIcon
 from PyQt5.QtWidgets import *
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
 
 app = QApplication([])
 
 SKIP = {
     "application/vnd.google-apps.audio",
-    "application/vnd.google-apps.video"
+    "application/vnd.google-apps.video",
+    "application/vnd.google-apps.shortcut"
 }
 
 MIMETYPES = {
@@ -25,11 +31,90 @@ MIMETYPES = {
     'application/vnd.google-apps.presentation':
         'application/vnd.openxmlformats-officedocument.presentationml.presentation'
 }
+
 EXTENSIONS = {
     'application/vnd.google-apps.document': '.docx',
     'application/vnd.google-apps.spreadsheet': '.xlsx',
     'application/vnd.google-apps.presentation': '.pptx'
 }
+
+SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.activity.readonly']
+
+
+def authenticate():
+    """Shows basic usage of the Drive v3 API.
+            Prints the names and ids of the first 10 files the user has access to.
+            """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+        return service
+    except HttpError as error:
+        print('An error occurred: {}'.format(error))
+
+
+def list_files_in_folder(folder_id, service):
+    return service.files().list(
+        q="'{}' in parents and trashed=false".format(folder_id),
+        fields="files(id, name, mimeType, parents)"
+    ).execute().get('files', [])
+
+
+def export_pdf(file_id, file_mime_type, file_path, service, text_box, level):
+    """
+    Download a Document file in PDF format.
+    :param file_id: file ID of any workspace document format file.
+    :param file_mime_type: MIME type of the file to be downloaded.
+    :param file_path: path to download the file to.
+    :param service: Drive API Client.
+    :param text_box: Text box to display the progress.
+    :param level: level of the file in the folder hierarchy.
+    :return: IO object with location
+    """
+
+    try:
+        if file_mime_type in SKIP:
+            return None
+        elif file_mime_type in MIMETYPES:
+            request = service.files().export_media(fileId=file_id,
+                                                   mimeType=MIMETYPES[file_mime_type])
+        else:
+            request = service.files().get_media(fileId=file_id)
+
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+            text_box.setTextColor(QtGui.QColor('#F2B90C'))
+            text_box.append('{}Download {}%.'.format(level * "\t", int(status.progress() * 100)))
+        file.seek(0)
+
+        with open(file_path, 'wb') as f:
+            shutil.copyfileobj(file, f)
+
+        text_box.setTextColor(QtGui.QColor('#1DC90E'))
+        text_box.append("{}Done\n".format(level * "\t"))
+
+    except HttpError as error:
+        print('An error occurred: {}'.format(error))
 
 
 class Widgets(QWidget):
@@ -105,9 +190,9 @@ class Widgets(QWidget):
             self,
             caption='Select a Folder'
         )
-        print(response)
-        self.t1.setText(response)
-        return response
+        self.t1.setText(os.path.normpath(response))
+        print(os.path.normpath(response))
+        return os.path.normpath(response)
 
     def init_ui(self):
         self.setWindowTitle('Sync Drive')
@@ -120,52 +205,51 @@ class Widgets(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    def upload_recur(self, table, path, level):
-        for x in table:
-            x_mime_type = x['mimeType']
-            x_path = os.path.join(path, x["title"])
-            if x_mime_type in MIMETYPES:
-                x_path = x_path + EXTENSIONS[x_mime_type]
+    def download_recursive(self, table, path, level):
+        """
 
-            x_path_split = x_path.split("\\")
+        :param table: folder data in drive.
+        :param path: path to download.
+        :param level: level of the file in the folder hierarchy.
+        :return: None.
+        """
+        for file in table:
+            file_mime_type = file['mimeType']
+            file_path = os.path.join(path, file["name"])
+            if file_mime_type in MIMETYPES:
+                file_path = file_path + EXTENSIONS[file_mime_type]
 
-            if x["mimeType"] == 'application/vnd.google-apps.folder':
+            x_path_split = file_path.split("\\")
+
+            if file["mimeType"] == 'application/vnd.google-apps.folder':
                 self.text_box.setTextColor(QtGui.QColor('#F2F2F2'))
                 self.text_box.append("{}{}".format(level * "\t", x_path_split[-1]))
                 app.processEvents()
-                if not os.path.exists(x_path):
+                if not os.path.exists(file_path):
                     self.text_box.setTextColor(QtGui.QColor('#F2B90C'))
                     self.text_box.append("{}Created {} folder in {}".format(level * "\t",
                                                                             x_path_split[-1],
                                                                             x_path_split[-2]))
                     app.processEvents()
 
-                    os.mkdir(x_path)
+                    os.mkdir(file_path)
 
-                x_id = x['id']
-                data_folder = self.drive.ListFile({'q': f"'{x_id}' in parents and trashed=false"}).GetList()
-                self.upload_recur(data_folder, x_path, level + 1)
+                file_id = file['id']
+                data_folder = list_files_in_folder(file_id, self.drive)
+                self.download_recursive(data_folder, file_path, level + 1)
             else:
-                if not os.path.exists(x_path):
+                if not os.path.exists(file_path):
                     self.text_box.setTextColor(QtGui.QColor('#F2B90C'))
                     self.text_box.append("{}Downloading {} to {}'s {}".format(level * "\t", x_path_split[-1],
                                                                               x_path_split[-3], x_path_split[-2]))
                     app.processEvents()
-
                     try:
-                        if x_mime_type in SKIP:
-                            return
-                        if x_mime_type in MIMETYPES:
-                            x.GetContentFile(x_path, mimetype=MIMETYPES[x_mime_type])
-                        else:
-                            x.GetContentFile(x_path)
-
-                        self.text_box.setTextColor(QtGui.QColor('#1DC90E'))
-                        self.text_box.append("{}Done\n".format(level * "\t"))
+                        export_pdf(file['id'], file['mimeType'], file_path, self.drive, self.text_box, level)
                         app.processEvents()
-                    except pydrive.files.FileNotDownloadableError:
-                        self.text_box.setTextColor(QtGui.QColor('#6CC6E5'))
-                        self.text_box.append("{}Can't be downloaded (Not an error)\n".format(level * "\t"))
+                    except ValueError as error:
+                        # self.text_box.setTextColor(QtGui.QColor('#6CC6E5'))
+                        # self.text_box.append("{}Can't be downloaded (Not an error)\n".format(level * "\t"))
+                        print(error)
                         app.processEvents()
 
     def run_driver(self):
@@ -184,23 +268,22 @@ class Widgets(QWidget):
             with open("cache.json", "w") as cache:
                 json.dump(data, cache)
 
-            g_auth = GoogleAuth()
-            self.drive = GoogleDrive(g_auth)
+            self.drive = authenticate()
 
-            data = self.drive.ListFile({'q': f"'{drive_id}' in parents and trashed=false"}).GetList()
+            data = list_files_in_folder(drive_id, self.drive)
 
             self.text_box.clear()
             self.text_box.moveCursor(QTextCursor.Start)
-            self.upload_recur(data, directory, 0)
+            self.download_recursive(data, directory, 0)
 
-            self.text_box.setTextColor(QtGui.QColor('#F2F2F2'))
+            self.text_box.setTextColor(QtGui.QColor('#282C34'))
             self.text_box.append("The End.")
 
 
 def run():
     style = """
             QWidget{
-                background: #262D37;
+                background: #282C34;
             }
             QLabel{
                 color: #fff;
@@ -213,7 +296,7 @@ def run():
             QPushButton
             {
                 color: white;
-                background: #0577a8;
+                background: #353B45;
                 border: 1px #DADADA solid;
                 padding: 5px 10px;
                 border-radius: 5px;
@@ -223,7 +306,7 @@ def run():
             QPushButton:hover{
                 border: 1px #C6C6C6 solid;
                 color: #fff;
-                background: #0892D0;
+                background: #414854;
             }
             QLineEdit {
                 padding: 1px;
